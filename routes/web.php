87385052503a755ad52d87bb464b1c022a01b265
HuyNguyen19989 <redwoodridofu@gmail.com>
2020-09-30 14:34:48 +0700

    git add .

--- routes/web.php
+++ routes/web.php
@@ -35,3 +35,7 @@ Route::get('/admin/newbook',function(){
 Route::post('/admin/newbook','admin@newbook');
 
 Route::get('/user/{name}/{password}', 'User@index')->where(['name' => '[a-zA-Z]+']);
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
